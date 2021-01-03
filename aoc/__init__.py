190ae1131da6ef8f9de5ea4f691464433a292a27
Wolfgang Welz <welzwo@gmail.com>
2021-01-03 17:23:02 +0100

    fix init

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -5,13 +5,15 @@ import sys
 __version__ = "2020.1"
 
 
-def plugin(year, day):
+def plugin(year, day, data):
     mod_name = f"aoc.{year}.day{day:02d}"
     sys.modules.pop(mod_name, None)
     old_stdout = sys.stdout
     sys.stdout = out = io.StringIO()
     try:
         runpy.run_module(mod_name, run_name="__main__")
+    except ImportError:
+        return None, None
     finally:
         sys.stdout = old_stdout
     lines = [x for x in out.getvalue().splitlines() if x]
